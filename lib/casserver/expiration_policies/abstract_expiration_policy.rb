4fbc8689be7cdca6fdf5854e56ef51b61363d89d
Tyler Pickett <t.pickett66@gmail.com>
2012-10-04 14:55:08 -0500

    implement expiration policy time limit setting
    There is a fair bit of obscurity here but not
    without reason! The interface for this and the
    rest of the expiration policies I think will
    largely be class methods so a module could
    work but it has its own downsides.
    
    Signed-off-by: Tyler Pickett <t.pickett66@gmail.com>

--- lib/casserver/expiration_policies/abstract_expiration_policy.rb
+++ lib/casserver/expiration_policies/abstract_expiration_policy.rb
@@ -1,7 +1,19 @@
 module CASServer
   module ExpirationPolicies
     class AbstractExpirationPolicy
+      @maximum_session_lifetime = nil
+      class << self
+        undef_method :new
 
+        attr_reader :maximum_session_lifetime
+
+        def maximum_session_lifetime=(session_lifetime)
+          @maximum_session_lifetime = session_lifetime
+          class << self
+            undef_method :maximum_session_lifetime=
+          end
+        end
+      end
     end
   end
 end
--- spec/casserver/expiration_policies/abstract_expiration_policy_spec.rb
+++ spec/casserver/expiration_policies/abstract_expiration_policy_spec.rb
@@ -4,4 +4,19 @@ require 'casserver/expiration_policies/abstract_expiration_policy'
 
 describe CASServer::ExpirationPolicies::AbstractExpirationPolicy do
 
+  context "setting the maximum_session_lifetime" do
+    before(:all) do
+      @policy = CASServer::ExpirationPolicies::AbstractExpirationPolicy
+      @max_lifetime = rand(1000000)
+      @policy.maximum_session_lifetime = @max_lifetime
+    end
+
+    it "should store the maximum_session lifetime at the class level" do
+      @policy.maximum_session_lifetime.should == @max_lifetime
+    end
+
+    it "should only be easy to set once" do
+      @policy.should_not respond_to :maximum_session_lifetime=
+    end
+  end
 end
