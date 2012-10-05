c2bf12e795ba8e7c19517edd533b0ad9779ab65e
Tyler Pickett <t.pickett66@gmail.com>
2012-10-05 10:52:15 -0500

    implement DefaultExpirationPolicy
    This exactly duplicates the logic contained in the current expired
    scope. That scope could probably be programatically generated for all
    ExpirationPolicies that are in use.

--- lib/casserver/expiration_policies/default_expiration_policy.rb
+++ lib/casserver/expiration_policies/default_expiration_policy.rb
@@ -3,7 +3,11 @@ require 'casserver/expiration_policies/abstract_expiration_policy'
 module CASServer
   module ExpirationPolicies
     class DefaultExpirationPolicy < AbstractExpirationPolicy
-
+      class << self
+        def ticket_expired?(ticket)
+          ticket.created_on < maximum_session_lifetime.seconds.ago
+        end
+      end
     end
   end
 end
