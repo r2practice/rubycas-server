a08e76876b369a85f39e023d3445adf152e4b8d7
Tyler Pickett <t.pickett66@gmail.com>
2012-10-05 11:57:08 -0500

    document the abstract expiration policy

--- lib/casserver/expiration_policies/abstract_expiration_policy.rb
+++ lib/casserver/expiration_policies/abstract_expiration_policy.rb
@@ -1,5 +1,10 @@
 require 'active_support'
 
+# @abstract Subclass and override #{ticket_expired?} to implement
+#   an ExpirationPolicy
+#
+# @author Tyler Pickett <tyler@therapylog.com>
+# @since ?
 module CASServer
   module ExpirationPolicies
     class AbstractExpirationPolicy
@@ -15,7 +20,15 @@ module CASServer
             undef_method :maximum_session_lifetime=
           end
         end
-
+        
+        # Entry point for determining if a ticket is expired
+        # This will enable our Tickets to delegate their
+        # expiration behavior to the various subclasses of this one
+        #
+        # @example Implement Ticket#expired?
+        # def expired?
+        #   expiration_policy.constantize.ticket_expired?(self)
+        # end
         def ticket_expired?(ticket)
           raise "ticket_expired? not implemented in AbstractExpirationPolicy, please override in #{self.name}"
         end
