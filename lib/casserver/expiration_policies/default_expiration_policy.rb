417c472a0b68964065c857a161649c86ee9414ee
Tyler Pickett <t.pickett66@gmail.com>
2012-10-05 11:57:28 -0500

    document DefaultExpirationPolicy

--- lib/casserver/expiration_policies/default_expiration_policy.rb
+++ lib/casserver/expiration_policies/default_expiration_policy.rb
@@ -1,9 +1,21 @@
 require 'casserver/expiration_policies/abstract_expiration_policy'
 
+# @see AbstractExpirationPolicy
+# Implements the original expiration policy logic based on the scopes
+# from the Model::Consumable module.
+#
+# @author Tyler Pickett <tyler@therapylog.com>
+# @since ?
 module CASServer
   module ExpirationPolicies
     class DefaultExpirationPolicy < AbstractExpirationPolicy
       class << self
+        
+        # Actually implement the logic for indicating a ticket as expired. 
+        # This policy is likely to be the most strict policy. Its
+        # implementation is virtually the identical to the JA-SIG
+        # implementation found in 
+        # org.jasig.cas.ticket.support.HardTimeoutExpirationPolicy
         def ticket_expired?(ticket)
           ticket.created_on < maximum_session_lifetime.seconds.ago
         end
