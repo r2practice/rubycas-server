0352c614a6d8f10cb358fb779b5caa7a641dd1f0
Tyler Pickett <t.pickett66@gmail.com>
2012-10-08 16:12:23 -0500

    first whack at RememberMe delegator
    we have mostly the same API here with the only
    significant difference being in the initialization
    code.
    
    Signed-off-by: Tyler Pickett <t.pickett66@gmail.com>

--- lib/casserver/expiration_policies/remember_me_expiration_policy.rb
+++ lib/casserver/expiration_policies/remember_me_expiration_policy.rb
@@ -1,6 +1,16 @@
 module CASServer
   module ExpirationPolicies
     class RememberMeExpirationPolicy
+      attr_reader :remember_me_policy, :standard_policy
+
+      def initialize(standard_policy, remember_me_policy)
+        @standard_policy = standard_policy
+        @remember_me_policy = remember_me_policy
+      end
+
+      def ticket_expired?(ticket)
+        (ticket.extra_attributes[:remember_me] ? remember_me_policy.ticket_expired?(ticket) : standard_policy.ticket_expired?(ticket))
+      end
     end
   end
 end
--- spec/casserver/expiration_policies/remember_me_expiration_policy_spec.rb
+++ spec/casserver/expiration_policies/remember_me_expiration_policy_spec.rb
@@ -7,4 +7,24 @@ require 'casserver/model'
 
 describe CASServer::ExpirationPolicies::RememberMeExpirationPolicy do
 
+  describe "#ticket_expired?" do
+    before do
+      @tgt = double(CASServer::Model::TicketGrantingTicket)
+      @standard_policy = double(CASServer::ExpirationPolicies::TimeoutExpirationPolicy)
+      @standard_policy.stub(:ticket_expired? => 'standard')
+      @remember_policy = double(CASServer::ExpirationPolicies::TimeoutExpirationPolicy)
+      @remember_policy.stub(:ticket_expired? => 'long')
+      @subject = CASServer::ExpirationPolicies::RememberMeExpirationPolicy.new(@standard_policy, @remember_policy)
+    end
+
+    it "should delegate to the standard policy if the ticket reports a falsey remember_me in extra_attributes" do
+      @tgt.stub(:extra_attributes => {})
+      @subject.ticket_expired?(@tgt).should == 'standard'
+    end
+
+    it "should delegate to the remember policy if the ticket reports a truthy remember_me in extra_attributes" do
+      @tgt.stub(:extra_attributes => { :remember_me => true })
+      @subject.ticket_expired?(@tgt).should == 'long'
+    end
+  end
 end
