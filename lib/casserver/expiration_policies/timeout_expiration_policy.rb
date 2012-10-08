85a042008dd3e9925d04ada6479e2999b1fa5d71
Tyler Pickett <t.pickett66@gmail.com>
2012-10-08 13:15:29 -0500

    implement expiration based on activity
    Signed-off-by: Tyler Pickett <t.pickett66@gmail.com>

--- lib/casserver/expiration_policies/timeout_expiration_policy.rb
+++ lib/casserver/expiration_policies/timeout_expiration_policy.rb
@@ -2,5 +2,8 @@ require 'casserver/expiration_policies/abstract_expiration_policy'
 
 module CASServer::ExpirationPolicies
   class TimeoutExpirationPolicy < AbstractExpirationPolicy
+    def ticket_expired?(ticket)
+      ticket.last_activity < maximum_ticket_lifetime.seconds.ago
+    end
   end
 end
--- spec/casserver/expiration_policies/timeout_expiration_policy_spec.rb
+++ spec/casserver/expiration_policies/timeout_expiration_policy_spec.rb
@@ -4,5 +4,26 @@ require 'casserver/expiration_policies/timeout_expiration_policy'
 require 'casserver/model'
 
 describe CASServer::ExpirationPolicies::TimeoutExpirationPolicy do
+  let(:subject){ CASServer::ExpirationPolicies::TimeoutExpirationPolicy.new(300) }
 
+  describe '#ticket_expired?(ticket)' do
+    before do
+      @tgt = double(CASServer::Model::TicketGrantingTicket)
+    end
+
+    it "should return true when ticket#last_activity is older than the timeout" do
+      @tgt.stub(:last_activity => 301.seconds.ago)
+      subject.ticket_expired?(@tgt).should be true
+    end
+
+    it "should return true when ticket#last_activity is exactly at the timeout" do
+      @tgt.stub(:last_activity => 300.seconds.ago)
+      subject.ticket_expired?(@tgt).should be true
+    end
+
+    it "should return false when ticket#last_activity is newer than the timeout" do
+      @tgt.stub(:last_activity => 299.seconds.ago)
+      subject.ticket_expired?(@tgt).should be false
+    end
+  end
 end
