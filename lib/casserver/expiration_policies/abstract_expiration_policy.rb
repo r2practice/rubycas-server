0e5152563b74cae59bac57fde38008384fc82738
Tyler Pickett <t.pickett66@gmail.com>
2012-10-08 11:44:43 -0500

    reimplement AbstractExpirationPolicy
    Rather than limit ourselves to a single timeout
    length for each implementation of the timeout
    logic we're going to limit the number of instances
    of each class we have to the number of timeout
    lengths we need. This is likely a premature
    optimization but it wasn't difficult to write and
    should provide a very compact/stable footprint
    once the code is warmed up.
    
    Signed-off-by: Tyler Pickett <t.pickett66@gmail.com>

--- lib/casserver/expiration_policies/abstract_expiration_policy.rb
+++ lib/casserver/expiration_policies/abstract_expiration_policy.rb
@@ -9,6 +9,28 @@ module CASServer
   module ExpirationPolicies
     class AbstractExpirationPolicy
 
+      attr_reader :maximum_ticket_lifetime
+
+      class << self
+        # Override object's .new method.
+        #
+        # What we're trying to accomplish is a bit of efficeny in handling
+        # these classes. We're likely to have very few different timeouts
+        # but will need a lot of references to the same timeout. Since
+        # nothing but the timeout is being stored the only part of this that
+        # has a potential race condition is the caching portion but that
+        # shouldn't prove to be a problem given that the extra instances
+        # will get reaped relatively quickly by the GC.
+        def new(maximum_ticket_lifetime)
+          @cache ||= {}
+          @cache[maximum_ticket_lifetime] ||= super(maximum_ticket_lifetime)
+        end
+      end
+
+      def initialize(maximum_ticket_lifetime)
+        @maximum_ticket_lifetime = maximum_ticket_lifetime
+      end
+
       # Entry point for determining if a ticket is expired
       # This will enable our Tickets to delegate their
       # expiration behavior to the various subclasses of this one
@@ -18,7 +40,7 @@ module CASServer
       #   expiration_policy.constantize.ticket_expired?(self)
       # end
       def ticket_expired?(ticket)
-        raise "ticket_expired? not implemented in AbstractExpirationPolicy, please override in #{self.name}"
+        raise "ticket_expired? not implemented in AbstractExpirationPolicy, please override in #{self.class.name}"
       end
     end
   end
--- spec/casserver/expiration_policies/abstract_expiration_policy_spec.rb
+++ spec/casserver/expiration_policies/abstract_expiration_policy_spec.rb
@@ -1,7 +1,33 @@
 require 'spec_helper'
 
 require 'casserver/expiration_policies/abstract_expiration_policy'
+require 'casserver/model'
 
 describe CASServer::ExpirationPolicies::AbstractExpirationPolicy do
 
+  let(:subject){ CASServer::ExpirationPolicies::AbstractExpirationPolicy.new(300) }
+
+  describe '.new' do
+    before do
+      @policy = CASServer::ExpirationPolicies::AbstractExpirationPolicy
+    end
+
+    it "should return the same instance when the same timeout is specified" do
+      # compares using object_id so we know we get the same one repeatedly
+      @policy.new(300).should be @policy.new(300)
+    end
+
+    it "should return different instances when different timeouts are specified" do
+      @policy.new(300).should_not be @policy.new(600)
+    end
+  end
+
+  describe '#ticket_expired?(ticket)' do
+    it 'should raise an error since this is the abstract class' do
+      expect {
+        ticket = double(CASServer::Model::Ticket)
+        subject.ticket_expired?(ticket)
+      }.to raise_error(RuntimeError)
+    end
+  end
 end
