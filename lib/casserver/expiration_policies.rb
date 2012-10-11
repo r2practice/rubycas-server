e7652963abad76f40b2bc91069c90b361910efee
Tyler Pickett <t.pickett66@gmail.com>
2012-10-11 16:25:19 -0500

    axe autoloading since it's not threadsafe
    We may want to do this loading via const_missing but for now we'll just
    pull in all of this code.

--- lib/casserver/expiration_policies.rb
+++ lib/casserver/expiration_policies.rb
@@ -5,9 +5,9 @@ end
 
 require 'casserver/expiration_policies/abstract_expiration_policy'
 
-CASServer::ExpirationPolicies.autoload :DefaultExpirationPolicy, 'casserver/expiration_policies/default_expiration_policy.rb'
-CASServer::ExpirationPolicies.autoload :RememberMeExpirationPolicy, 'casserver/expiration_policies/remember_me_expiration_policy.rb'
-
-# Commented out because we don't have a last_activity column yet on any of the tickets
-# CASServer::ExpirationPolicies.autoload :TimeoutExpirationPolicy, 'casserver/expiration_policies/timeout_expiration_policy.rb'
+# We'll also need to pull in the Timeout based one eventually but 
+# since we don't have the column in place yet I'll hold off
+%w{default_expiration_policy remember_me_expiration_policy}.each do |policy|
+  require "casserver/expiration_policies/#{policy}"
+end
 
