ac80beeca2e14e6999324757f7841efde23a37aa
Tyler Pickett <t.pickett66@gmail.com>
2012-10-11 15:46:01 -0500

    make expiration policies easier to load
    Signed-off-by: Tyler Pickett <t.pickett66@gmail.com>

--- lib/casserver/expiration_policies.rb
+++ lib/casserver/expiration_policies.rb
@@ -1,2 +1,13 @@
-module CASServer::ExpirationPolicies; end
+module CASServer
+  module ExpirationPolicies
+  end
+end
+
+require 'casserver/expiration_policies/abstract_expiration_policy'
+
+CASServer::ExpirationPolicies.autoload :DefaultExpirationPolicy, 'casserver/expiration_policies/default_expiration_policy.rb'
+CASServer::ExpirationPolicies.autoload :RememberMeExpirationPolicy, 'casserver/expiration_policies/remember_me_expiration_policy.rb'
+
+# Commented out because we don't have a last_activity column yet on any of the tickets
+# CASServer::ExpirationPolicies.autoload :TimeoutExpirationPolicy, 'casserver/expiration_policies/timeout_expiration_policy.rb'
 
